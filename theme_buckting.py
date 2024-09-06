2378f9170d8ef81d00c5a2ca77c4fd804892c14c
Rafayel Latif <Rafayel.latif@gmail.com>
2024-09-06 17:29:38 +0100

    name updates

--- theme_buckting.py
+++ theme_buckting.py
@@ -17,29 +17,25 @@ EMBEDDING_MODEL = "text-embedding-3-small"
 
 max_distance = 0.7
 
-word2 = input("input genre: ")
+word2 = input("input theme: ")
 Dis_list = []
 
 for theme in Theme:
-    print("genre: ", theme)
+    print("theme: ", theme)
     distance = nearest_word(theme, word2)
     Dis_list.append((theme, distance))
 
 # Sort by distance
 Dis_list.sort(key=lambda x: x[1])
 
-# Print the results
-for genre, distance in Dis_list:
-    print(f"Genre: {genre}, Distance: {distance}")
-
-# Find the closest genre
+# Find the closest 
 closest_distance = Dis_list[0]
 print(closest_distance)
-closest_genre = Dis_list[0][0]
+closest_theme = Dis_list[0][0]
 if closest_distance[1]>max_distance:
     print("make new bucket for :", word2)
     embedding_model_test.new_bucket(word2)
     print("Sucessfully made new bucket for :", word2)
 else:
-    print(f"The closest genre is: {closest_genre}")
-    # we here need to add the genre to the list of closest genres
+    print(f"The closest theme is: {closest_theme}")
+    # we here need to add the theme to the list of closest themes
