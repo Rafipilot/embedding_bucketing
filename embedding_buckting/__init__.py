51d92c5feb9050ce156bc6d4dc939a4d511481d8
Rafayel Latif <Rafayel.latif@gmail.com>
2024-09-11 18:01:36 +0100

    bug fix

--- embedding_buckting/__init__.py
+++ embedding_buckting/__init__.py
@@ -1,7 +1,7 @@
 # embedding_buckting/__init__.py
 
 # Import specific classes or functions to make them available at the package level
-from .embedding_model_test import embedding_buckting
+from .embedding_model_test import EmbeddingModel
 
 
 # You can also specify which modules should be exposed
