2943e80bf4f06ca5527813ed2c6854fda135221a
Rafayel Latif <Rafayel.latif@gmail.com>
2024-09-11 15:02:02 +0100

    pls work

--- Module/__init__.py
+++ Module/__init__.py
@@ -1,11 +1,8 @@
 # embedding_buckting/__init__.py
 
 # Import specific classes or functions to make them available at the package level
-from .embedding_model_test import EmbeddingModel
-from .comparitive_title_query import ComparativeTitleQuery
-from .config import Config
-from .genre_query import GenreQuery
-from .theme_buckting import ThemeBuckting
+from .embedding_model_test import *
+
 
 # You can also specify which modules should be exposed
 __all__ = [
